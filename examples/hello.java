e2dbc93423b574636a39c115258c44f0f9b71849
aarizaf. <aarizaf@uninorte.edu.co>
2025-03-07 10:07:29 -0500

    hello

--- examples/hello.java
+++ examples/hello.java
@@ -1,5 +1,5 @@
 // FibonacciSum.java
-public class Hello {
+public class hello {
     public static long fibonacciSum(int n) {
         long a = 0, b = 1, total = 0;
         for (int i = 0; i < n; i++) {
